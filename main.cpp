ff34eb2d74ef8295f3450cc7cbd4007cdc40221f
Codeissimple <>
2021-04-06 23:57:05 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+int i = 1;
+  while (i <= 25){
+    cout << "Hello World! " << "- Printed " << ++i << " time." << endl;
+//    i++;
+  }
+return 0;
+}
